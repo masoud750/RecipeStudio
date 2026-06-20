a1349d97abb12ee238bdc912f65ef7aa185939b0
Masoud <246682095+masoud750@users.noreply.github.com>
2026-06-20 14:37:03 +0200

    test: add unit test for StepVM.cs

---
+++ RecipeStudioGUITests/ViewModels/StepVMTests.cs
@@ -0,0 +1,25 @@
+﻿using RecipeStudio.Domain.Entities;
+using RecipeStudio.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeStudio.UI.Tests.ViewModels
+{
+    [TestFixture]
+    internal class StepVMTests
+    {
+        [Test]
+        public void Constructor_WhenDMIsNull_ThrowsArgNullEx()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                _ = new StepVM(null!);
+            });
+
+            Assert.That(ex.ParamName, Is.EqualTo("dm"));
+        }
+    }
+}
