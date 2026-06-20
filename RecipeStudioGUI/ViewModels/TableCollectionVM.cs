eb496e22c50374bfd3e8d205eeb2625f6f4109ec
Masoud <246682095+masoud750@users.noreply.github.com>
2026-06-20 14:33:07 +0200

    test: add unit tests for TableCollectionVM

---
+++ RecipeStudioGUI/ViewModels/TableCollectionVM.cs
@@ -0,0 +1,221 @@
+﻿using RecipeStudio.Domain.Entities;
+using RecipeStudio.UI.ViewModels;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Windows;
+namespace RecipeStudio.UI.ViewModels
+{
+    public class TableCollectionVM : ModelBase  
+    {
+
+        // Constructor injection
+        public TableCollectionVM(CategoryTableVM categoryVM, 
+                                    RecipeTableVM recipeVM, StepTableVM stepVM,
+                                        IngredientTableVM ingredientVM,
+                                    RecipeQueryTableVM queryVM,
+                                        Helpers.IMessageService msgService)
+        {
+            ArgumentNullException.ThrowIfNull(categoryVM, nameof(categoryVM));
+            ArgumentNullException.ThrowIfNull(recipeVM, nameof(recipeVM));
+            ArgumentNullException.ThrowIfNull(stepVM, nameof(stepVM));
+            ArgumentNullException.ThrowIfNull(ingredientVM, nameof(ingredientVM));
+            ArgumentNullException.ThrowIfNull(queryVM, nameof(queryVM));
+            ArgumentNullException.ThrowIfNull(msgService, nameof(msgService));
+            Category = categoryVM;
+            Recipe = recipeVM;
+            Step = stepVM;
+            _msgService = msgService;
+            Ingredient = ingredientVM;
+            Query = queryVM;
+            Recipe.AllCategories = Category.CategoryModels;
+            Recipe.AllIngredients = Ingredient.Ingredients;
+            Recipe.AllSteps = Step.Steps;           
+        }
+
+       
+        public CategoryTableVM Category { get; } 
+        public RecipeTableVM Recipe { get; }
+        public StepTableVM Step { get; }
+
+        public RecipeQueryTableVM Query { get;}
+
+        public IngredientTableVM Ingredient { get; }
+        private Helpers.IMessageService _msgService;
+
+        //public async Task AddCategoryAsync(CategoryDM category) => await CategoryVM.AddAsync(category);
+        public async Task AddRecipeAsync(RecipeDM recipe) => 
+            await Recipe.AddAsync(recipe);
+        public async Task AddStepAsync(StepDM step) => 
+            await Step.AddAsync(step);
+        public async Task AddStepAsync(IngredientDM ingredient) => 
+            await Ingredient.AddAsync(ingredient);
+
+        public async Task InitializeAsync()
+        {
+            try
+            {
+                await Category.InitializeAsync();
+
+                //await Recipe.InitializeAsync();
+                Recipe.AllCategories = Category.CategoryModels;
+                Category.Categories.CollectionChanged += UpdateCategoryFilter;
+                Category.EditCategory += UpdateCategoryNamesInFilter;
+                await Step.InitializeAsync();
+                await Ingredient.InitializeAsync();            
+                PopulateCategoryFilter();
+                PopulateIngredientFilter();
+                
+
+            }
+            catch (Exception ex)
+            {
+                _msgService.ShowMessage($"Initialization failed: {ex.Message}", 
+                    Helpers.MessageType.Warning, "Warning");
+            }
+        }
+
+        
+
+
+        private void PopulateCategoryFilter()
+        {
+            Query.Categories.Clear();
+            Query.Categories.Add("Select an item...");
+            foreach (var item in Category.Categories)
+            {
+                Query.Categories.Add(item.Name);
+            }
+        }
+
+        private void PopulateIngredientFilter()
+        {
+            Query.Ingredients.Clear();
+            Query.Ingredients.Add("Select an item...");
+            foreach (var item in Ingredient.Ingredients)
+            {
+                Query.Ingredients.Add(item.Name);
+            }
+        }
+
+        public void PopulateUsers()
+        {
+            Query.Users.Clear();
+            Query.Users.Add("Select an item...");
+            foreach (var item in Recipe.Recipes)
+            {
+
+                if(!Query.Users.Any( x => x == item.User.UserName))
+                    Query.Users.Add(item.User.UserName);
+            }
+        }
+
+        private void UpdateCategoryNamesInFilter(object? sender, EventArgs e)
+        {
+           if(sender is CategoryTableVM vm)
+            {
+                PopulateCategoryFilter();
+
+            }
+        }
+
+        private void UpdateCategoryFilter(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (sender is ObservableCollection<CategoryVM>)
+            {
+                if( e.Action == NotifyCollectionChangedAction.Add)//CollectionChangeAction.Add)
+                {
+                    Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        List<CategoryVM> items = e.NewItems.Cast<CategoryVM>().ToList();
+                        if (items.Count > 0)
+                        {
+                            Query.Categories.Add(item: items[0].Name);
+                        }
+
+                    }));
+                   
+                }
+
+                if (e.Action == NotifyCollectionChangedAction.Remove)//CollectionChangeAction.Add)
+                {
+                    PopulateCategoryFilter();
+
+                }
+            }
+        }
+
+        public string ColumnHeader { get; set; } = string.Empty;
+
+ 
+
+        public string? ItemToAdd
+        {
+            get
+            {
+                return _itemToAdd;
+            }
+            set
+            {
+                if(!string.IsNullOrEmpty(value) && _itemToAdd != value)
+                {
+                    _itemToAdd = value;
+                }
+               
+            }
+        }
+
+        public string? _itemToAdd;
+
+        // Async Commands
+        //public ICommand AddCommand => new RelayCommand(async _ =>
+        //{
+        //    try
+        //    {
+
+
+        //        if (string.IsNullOrWhiteSpace(_itemToAdd))
+        //            return;
+
+        //        // ✅ now valid
+
+        //        await _repository!.AddAsync(newItem).ConfigureAwait(false);
+        //        await _repository!.SaveChangesAsync().ConfigureAwait(false);
+        //        Items.Add(newItem);
+        //        _itemToAdd = string.Empty;
+        //        OnPropertyChanged(nameof(ItemToAdd));
+
+
+
+        //        // Nur wenn erfolgreich, UI aktualisieren
+        //        Items.Add(newItem);
+        //        _messageService?.ShowMessage("New entry created successfully.", Helpers.MessageType.Info);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _messageService?.ShowMessage($"$\"Error adding item: {ex.Message}", Helpers.MessageType.Error);
+
+        //    }
+        //});
+
+        //public ICommand UpdateCommand => new RelayCommand(async _ =>
+        //{
+        //    if (SelectedItem != null)
+        //    {
+        //        await _repository!.UpdateAsync(SelectedItem);
+        //        // UI updates automatically if SelectedItem properties implement INotifyPropertyChanged
+        //    }
+        //});
+
+        //public ICommand DeleteCommand => new RelayCommand(async _ =>
+        //{
+        //    if (SelectedItem != null)
+        //    {
+        //        //await _repository!.DeleteAsync(SelectedItem);
+        //        //Items.Remove(SelectedItem);
+        //    }
+        //});
+    }
+
+
+}
---
+++ RecipeStudioGUITests/ViewModels/TableCollectionVMTests.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeStudio.DataAccess.Data;
+using RecipeStudio.Domain.Entities;
+using RecipeStudio.Repository.Presistence;
+using RecipeStudio.UI.Tests.Mocks;
+using RecipeStudio.UI.ViewModels;
+using RecipeStudioUI.Tests.Helpers;
+using RecipeStudioUI.Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeStudio.UI.Tests.ViewModels
+{
+    [TestFixture]
+    internal class TableCollectionVMTests
+    {
+        [Test]
+        public void Constructor_WhenCategoryVMIsNull_ThrowsArgNullEx()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+                      {
+                          var recipeTableVm = new RecipeTableVM(
+                                                    new FakeRecipeRepository(),
+                                                    new MessageServiceMock());
+                          var stepTableVM = new StepTableVM(
+                                                    new FakeStepRepository(),
+                                                    new MessageServiceMock());
+
+                          var ingredientTableVM = new IngredientTableVM(
+                                                    new FakeIngredientRepository(),
+                                                    new MessageServiceMock());
+
+                          var recipeQueryTable = new RecipeQueryTableVM(
+                                                        new RecipeRepositoryMock(),
+                                                        new MessageServiceMock());
+
+                          _ = new TableCollectionVM(null!, recipeTableVm, stepTableVM,
+                                    ingredientTableVM, recipeQueryTable,
+                                    new MessageServiceMock());
+                      });
+            Assert.That(ex.ParamName, Is.EqualTo("categoryVM"));
+        }
+
+        [Test]
+        public void Constructor_WhenRecipeTableVMIsNull_ThrowsArgNullEx()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var dbContext = TestHelper.GetDbContext();
+
+                var categoryTableVM = new CategoryTableVM(
+                    new CategoryRepository(dbContext),
+                    new MessageServiceMock()
+                    );
+                var recipeTableVm = new RecipeTableVM(
+                                                   new FakeRecipeRepository(),
+                                                   new MessageServiceMock());
+                var stepTableVM = new StepTableVM(
+                                          new FakeStepRepository(),
+                                          new MessageServiceMock());
+
+                var ingredientTableVM = new IngredientTableVM(
+                                          new FakeIngredientRepository(),
+                                          new MessageServiceMock());
+
+                var recipeQueryTable = new RecipeQueryTableVM(
+                                              new RecipeRepositoryMock(),
+                                              new MessageServiceMock());
+
+                _ = new TableCollectionVM(categoryTableVM, null!, stepTableVM,
+                          ingredientTableVM, recipeQueryTable,
+                          new MessageServiceMock());
+            });
+            Assert.That(ex.ParamName, Is.EqualTo("recipeVM"));
+        }
+
+        [Test]
+        public void Constructor_WhenStepTableVMIsNull_ThrowsArgNullEx()
+        {
+
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+                 {
+                     var dbContext = TestHelper.GetDbContext();
+
+                     var categoryTableVM = new CategoryTableVM(
+                         new CategoryRepository(dbContext),
+                         new MessageServiceMock()
+                         );
+                     var recipeTableVm = new RecipeTableVM(
+                                                        new FakeRecipeRepository(),
+                                                        new MessageServiceMock());
+
+                     var ingredientTableVM = new IngredientTableVM(
+                                               new FakeIngredientRepository(),
+                                               new MessageServiceMock());
+
+                     var recipeQueryTable = new RecipeQueryTableVM(
+                                                   new RecipeRepositoryMock(),
+                                                   new MessageServiceMock());
+
+
+                     _ = new TableCollectionVM(categoryTableVM, recipeTableVm,
+                                                 null!, ingredientTableVM,
+                                                             recipeQueryTable,
+                                                       new MessageServiceMock());
+                 });
+
+            Assert.That(ex.ParamName, Is.EqualTo("stepVM"));
+        }
+
+        [Test]
+        public void Constructor_WhenIngredientTableVMIsNull_ThrowsArgNullEx()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var dbContext = TestHelper.GetDbContext();
+
+                var categoryTableVM = new CategoryTableVM(
+                    new CategoryRepository(dbContext),
+                    new MessageServiceMock()
+                    );
+                var recipeTableVm = new RecipeTableVM(
+                                                   new FakeRecipeRepository(),
+                                                   new MessageServiceMock());
+                var stepTableVM = new StepTableVM(
+                                          new FakeStepRepository(),
+                                          new MessageServiceMock());
+
+                var recipeQueryTable = new RecipeQueryTableVM(
+                                              new RecipeRepositoryMock(),
+                                              new MessageServiceMock());
+
+                _ = new TableCollectionVM(categoryTableVM, recipeTableVm,
+                                            stepTableVM, null!,
+                                                        recipeQueryTable,
+                                                  new MessageServiceMock());
+            });
+            Assert.That(ex.ParamName, Is.EqualTo("ingredientVM"));
+        }
+
+        [Test]
+        public void Constructor_WhenQueryTableVMIsNull_ThrowsArgNullEx()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var dbContext = TestHelper.GetDbContext();
+
+                var categoryTableVM = new CategoryTableVM(
+                    new CategoryRepository(dbContext),
+                    new MessageServiceMock()
+                    );
+                var recipeTableVm = new RecipeTableVM(
+                                                   new FakeRecipeRepository(),
+                                                   new MessageServiceMock());
+                var stepTableVM = new StepTableVM(
+                                          new FakeStepRepository(),
+                                          new MessageServiceMock());
+
+                var ingredientTableVM = new IngredientTableVM(
+                                          new FakeIngredientRepository(),
+                                          new MessageServiceMock());
+                _ = new TableCollectionVM(categoryTableVM, recipeTableVm, 
+                                            stepTableVM, ingredientTableVM,
+                                              null!,
+                                                  new MessageServiceMock());
+            });
+            Assert.That(ex.ParamName, Is.EqualTo("queryVM"));
+        }
+
+
+        [Test]
+        public void Constructor_WhenMsgServiceIsNull_ThrowsArgNullEx()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var dbContext = TestHelper.GetDbContext();
+
+                var categoryTableVM = new CategoryTableVM(
+                    new CategoryRepository(dbContext),
+                    new MessageServiceMock()
+                    );
+                var recipeTableVm = new RecipeTableVM(
+                                                   new FakeRecipeRepository(),
+                                                   new MessageServiceMock());
+                
+                var stepTableVM = new StepTableVM(
+                                          new FakeStepRepository(),
+                                          new MessageServiceMock());
+                                
+                
+                var ingredientTableVM = new IngredientTableVM(
+                                          new FakeIngredientRepository(),
+                                          new MessageServiceMock());
+
+                var recipeQueryTable = new RecipeQueryTableVM(
+                                             new RecipeRepositoryMock(),
+                                             new MessageServiceMock());
+
+                _ = new TableCollectionVM(categoryTableVM, recipeTableVm,
+                                            stepTableVM, ingredientTableVM,
+                                              recipeQueryTable,
+                                                  null!);
+            });
+            Assert.That(ex.ParamName, Is.EqualTo("msgService"));
+        }
+    }
+}
