788cde12a9e4bf8286f9d87ecc0043246e5c34d1
Masoud <246682095+masoud750@users.noreply.github.com>
2026-06-20 14:35:50 +0200

    test: add unit test for RecipeQueryTableVM.cs

---
+++ RecipeStudio.Repository/Persistence/RecipeQueryRepository.cs
@@ -0,0 +1,73 @@
+﻿
+using RecipeStudio.DataAccess.Data;
+using RecipeStudio.Domain.Entities;
+using RecipeStudio.Repository.Interfaces;
+
+namespace RecipeStudio.Repository.Presistence
+{
+    public class RecipeQueryRepository : IRepository<RecipeDM>
+    {
+
+
+        public RecipeQueryRepository(RecipesDBContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        private RecipesDBContext _context;
+
+        public Task AddAsync(RecipeDM entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<RecipeDM>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RecipeDM?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(RecipeDM entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RecipeDM?> GetByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(RecipeDM entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RecipeDM? GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddFavoriteAsync(int userId, int recipeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
